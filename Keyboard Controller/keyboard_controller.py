c4a95f14626f6919a41f4cdc67a36efd9035f034
Ali EFE <78353929+Aliefe02@users.noreply.github.com>
2024-01-14 14:31:25 +0300

    Update keyboard_controller.py

--- Keyboard Controller/keyboard_controller.py
+++ Keyboard Controller/keyboard_controller.py
@@ -7,21 +7,23 @@ ser = serial.Serial('COM10', 9600)
 while True:
     command = ""
     try:
-        if keyboard.is_pressed("w"):
+        if keyboard.is_pressed("l"):    # Launch
+            command += "L"
+        if keyboard.is_pressed("w"):    # Forward
             command += "W"
-        elif keyboard.is_pressed("s"):
+        elif keyboard.is_pressed("s"):  # Backward
             command += "S"
-        if keyboard.is_pressed("a"):
+        if keyboard.is_pressed("a"):    # Left
             command += "A"
-        elif keyboard.is_pressed("d"):
+        elif keyboard.is_pressed("d"):  # Right
             command += "D"
-        if keyboard.is_pressed("e"):
+        if keyboard.is_pressed("e"):    # Yaw CW
             command += "E"
-        elif keyboard.is_pressed("q"):
+        elif keyboard.is_pressed("q"):  # Yaw CCW
             command += "Q"
-        if keyboard.is_pressed("u"):
+        if keyboard.is_pressed("u"):    # Ascend
             command += "U"
-        elif keyboard.is_pressed("j"):
+        elif keyboard.is_pressed("j"):  # Descend
             command += "J"
 
         print(command)
