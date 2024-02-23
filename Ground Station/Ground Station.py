cf68c8004baa72c5fb36dd7db0526ee596bf8f59
Ali EFE <78353929+Aliefe02@users.noreply.github.com>
2024-02-24 00:36:13 +0300

    Add files via upload

---
+++ Ground Station/Ground Station.py
@@ -0,0 +1,120 @@
+import tkinter as tk
+import serial
+
+#ser = serial.Serial('COM13', 9600)
+class DrawingApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Drawing App")
+
+        # Parameters on the right
+        self.param_frame = tk.Frame(master, padx=10)
+        self.param_frame.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # Create labels and textboxes for parameters
+        self.param_entries = []
+
+        label = tk.Label(self.param_frame, text='Height')
+        label.pack(pady=2)
+        entry = tk.Entry(self.param_frame)
+        entry.insert(0, 2)
+        entry.pack(pady=5)
+        self.param_entries.append(entry)
+
+
+        label = tk.Label(self.param_frame, text='Cell Length')
+        label.pack(pady=2)
+        entry = tk.Entry(self.param_frame)
+        entry.insert(0, 1)
+        entry.pack(pady=5)
+        self.param_entries.append(entry)
+
+        # Buttons to trigger actions
+        self.draw_button = tk.Button(self.param_frame, text="Start Drone", command=self.draw)
+        self.draw_button.pack(pady=5)
+
+        self.reset_button = tk.Button(self.param_frame, text="Reset", command=self.reset_drawings)
+        self.reset_button.pack(pady=5)
+
+        # Canvas on the left
+        self.canvas = tk.Canvas(master, bg="white")
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # Initialize grid size
+        self.grid_size = 50
+
+        # Drawn squares and initial grid
+        self.drawn_squares = []
+        self.master.after(100, self.init_grid)  # Schedule init_grid after a short delay
+
+        # Event bindings for drawing
+        self.canvas.bind("<B1-Motion>", self.draw_on_canvas)
+        self.canvas.bind("<ButtonRelease-1>", self.release_button)
+
+        # Bind resizing event
+        self.master.bind("<Configure>", self.redraw_grid)
+
+    def init_grid(self):
+        for i in range(0, self.canvas.winfo_width(), max(1, self.canvas.winfo_width() // self.grid_size)):
+            self.canvas.create_line(i, 0, i, self.canvas.winfo_height(), fill="gray", dash=(2, 2), tags="grid")
+            self.canvas.create_line(0, i, self.canvas.winfo_width(), i, fill="gray", dash=(2, 2), tags="grid")
+
+    def draw_on_canvas(self, event):
+        x, y = event.x, event.y
+        square_size = self.canvas.winfo_width() // self.grid_size
+        col = x // square_size
+        row = y // square_size
+
+        square_coords = (col, row)
+        if self.drawn_squares == [] or  self.drawn_squares[-1] != square_coords:
+            self.drawn_squares.append(square_coords)
+            #print(square_coords)
+
+        # Draw on the canvas
+        x1 = col * square_size
+        y1 = row * square_size
+        x2 = x1 + square_size
+        y2 = y1 + square_size
+
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill="black")
+
+    def release_button(self, event):
+        pass  # Add any cleanup or additional actions here
+
+    def draw(self):
+        if self.drawn_squares == []:    # If no path drawn, dont send
+            return
+        height = self.param_entries[0].get()
+        cell_length = self.param_entries[1].get()
+        # Call send_path_to_drone with the parameters and drawn squares
+        send_path_to_drone(self.drawn_squares, height, cell_length)
+
+    def redraw_grid(self, event=None):
+        self.canvas.delete("grid")
+        for i in range(0, self.canvas.winfo_width(), max(1, self.canvas.winfo_width() // self.grid_size)):
+            self.canvas.create_line(i, 0, i, self.canvas.winfo_height(), fill="gray", dash=(2, 2), tags="grid")
+            self.canvas.create_line(0, i, self.canvas.winfo_width(), i, fill="gray", dash=(2, 2), tags="grid")
+
+    def reset_drawings(self):
+        # Clear drawn squares and the canvas
+        self.drawn_squares.clear()
+        self.canvas.delete("all")
+        # Redraw the grid after resetting
+        self.redraw_grid()
+
+def send_path_to_drone(path, height, cell_length):
+    data = {'path':path,'height':height,'cell_length':cell_length,'check':'correct'}
+    print(str(data))
+    #ser.write(str(data).encode())
+    #ser.write(b'\n')
+
+
+def main():
+    
+    root = tk.Tk()
+    root.geometry("1200x600")
+    app = DrawingApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ Ground Station/drone.py
@@ -0,0 +1,359 @@
+
+import time
+time.sleep(2)
+
+import sys
+sys.stdout = open('/home/ali/Desktop/stdout.log', 'w')
+sys.stderr = open('/home/ali/Desktop/stderr.log', 'w')
+
+from dronekit import connect,VehicleMode,LocationGlobalRelative,APIException
+import socket
+import math
+import argparse
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from SX127x.LoRa import *
+from SX127x.board_config import BOARD
+from pymavlink import mavutil
+
+targetHeight = 2
+def remove_null_bytes(data):
+    return data.replace('\x00', '')
+
+targetAltitude = 1
+manualArm = False
+
+# LoRa setup
+
+BOARD.setup()
+RST = None
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+disp.begin()
+disp.clear()
+disp.display()
+
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+draw = ImageDraw.Draw(image)
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+font = ImageFont.load_default()
+
+
+def draw_on_display(data):
+    global data_write
+    global test
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((5, 10), "> "+data, font=font, fill=255)
+    rotated_image = image.rotate(180)  # Rotate the image by 180 degrees
+    disp.image(rotated_image)
+    disp.display()
+
+def draw_movement(x,y,z,yaw):
+    global data_write
+    global test
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((5, 10), " "+str(x)+" "+str(y)+" "+str(z)+" "+str(yaw), font=font, fill=255)
+    rotated_image = image.rotate(180)  # Rotate the image by 180 degrees
+    disp.image(rotated_image)
+    disp.display()
+
+
+
+def connectMyCopter():
+    print("Starting the connection")
+    connection_string = "/dev/ttyACM0"
+    baud_rate = 57600
+
+    vehicle = connect(connection_string,baud=baud_rate,wait_ready=True)
+    print("Conected")
+    return vehicle
+
+def arm():
+    print("Arming")
+    vehicle.mode = VehicleMode("GUIDED")
+    while vehicle.mode != 'GUIDED':
+        time.sleep(1)
+        print("Waiting for drone to enter GUIDED mode")
+    vehicle.armed = True
+    while vehicle.armed== False:
+        print("Waiting for drone to become armed..")
+        vehicle.armed = True
+        time.sleep(1)
+    print("\n[Vehicle is now armed]")
+    print("[Props are spinning]")
+    return None
+
+def loiter():
+    print('Arming Vehicle')
+
+    vehicle.mode = VehicleMode("GUIDED")
+
+    while vehicle.mode != 'GUIDED':
+        print("Waiting for drone to enter GUIDED mode")
+        time.sleep(1)
+    print("Vehicle in GUIDED mode")
+
+    if manualArm == False:
+        vehicle.armed = True
+        while vehicle.armed == False:
+            print("Waiting for vehicle to become armed")
+            time.sleep(1)
+    else:
+        if vehicle.armed == False:
+            print("Exiting script. Failed when arming")
+            return None
+    print("Vehicle is ARMED")
+    draw_on_display("Flying")
+    vehicle.simple_takeoff(2)
+
+    while True:
+        print("Current Altitude: "+str(vehicle.location.global_relative_frame.alt))
+        if vehicle.location.global_relative_frame.alt >= .95*targetHeight:
+            break
+        time.sleep(0.5)
+    print("Target altitude reached!")
+    vehicle.mode = VehicleMode("LOITER")
+    return None
+
+
+def hover():
+    print('Arming Vehicle')
+
+    vehicle.mode = VehicleMode("GUIDED")
+
+    while vehicle.mode != 'GUIDED':
+        print("Waiting for drone to enter GUIDED mode")
+        time.sleep(1)
+    print("Vehicle in GUIDED mode")
+
+    if manualArm == False:
+        vehicle.armed = True
+        while vehicle.armed == False:
+            print("Waiting for vehicle to become armed")
+            time.sleep(1)
+    else:
+        if vehicle.armed == False:
+            print("Exiting script. Failed when arming")
+            return None
+    print("Vehicle is ARMED")
+    draw_on_display("Flying")
+    vehicle.simple_takeoff(2)
+
+    while True:
+        print("Current Altitude: "+str(vehicle.location.global_relative_frame.alt))
+        if vehicle.location.global_relative_frame.alt >= .95*targetHeight:
+            break
+        time.sleep(0.5)
+    print("Target altitude reached!")
+
+    return None
+
+def move(x, y, z,yaw):
+    draw_movement(x,y,z,yaw)
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,
+        0, 0,
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,
+        0b0000011111000111,
+        0, 0, 0,
+        x, y, z,
+        0, 0, 0,
+        0, math.radians(yaw))
+
+    vehicle.send_mavlink(msg)
+    time.sleep(1)
+
+def take_off(height):
+    print('Arming Vehicle')
+
+    vehicle.mode = VehicleMode("GUIDED")
+
+    while vehicle.mode != 'GUIDED':
+        print("Waiting for drone to enter GUIDED mode")
+        time.sleep(.5)
+    print("Vehicle in GUIDED mode")
+
+    if manualArm == False:
+        vehicle.armed = True
+        while vehicle.armed == False:
+            print("Waiting for vehicle to become armed")
+            time.sleep(1)
+    else:
+        if vehicle.armed == False:
+            print("Exiting script. Failed when arming")
+            return None
+    print("Vehicle is ARMED")
+    vehicle.simple_takeoff(int(height))
+    draw_on_display("Flying")
+    while True:
+        print("Current Altitude: "+str(vehicle.location.global_relative_frame.alt))
+        if vehicle.location.global_relative_frame.alt >= .95*2:
+            break
+        time.sleep(0.5)
+    print("Target altitude reached!")
+
+    return None
+
+def point_forward(degree):
+    rotation = 0
+    if degree != 0:
+        if degree == 90:
+            rotation = -90
+        elif degree == 270:
+            rotation = 90
+        elif degree == 180:
+            rotation = 180
+        move(0,0,0,rotation)
+    return 0  
+  
+def point_right(degree):
+    rotation = 0
+    if degree != 90:
+        if degree == 0:     
+            rotation = 90
+        elif degree == 180:
+            rotation = -90
+        elif degree == 270:
+            rotation = 180
+        
+        move(0,0,0,rotation)
+    return 90
+
+def point_backwards(degree):
+    rotation = 0
+    if degree != 180:
+        if degree == 0:
+            rotation = 180
+        elif degree == 90:
+            rotation = 90
+        elif degree == 270:
+            rotation = -90
+        move(0,0,0,rotation)
+    return 180
+
+def point_left(degree):
+    rotation = 0
+    if degree != 270:
+        if degree == 0:
+            rotation = -90
+        elif degree == 90:
+            rotation = 180
+        elif degree == 180:
+            rotation = 90
+        move(0,0,0,rotation)
+    return 270
+
+def go_forward():
+    move(1,0,0,0)
+
+def land():
+    vehicle.mode = VehicleMode("LAND")
+    while vehicle.mode!='LAND':
+        time.sleep(1)
+        draw_on_display("Landing")
+        print("Waiting for drone to land")
+    draw_on_display("Drone Landed")
+
+def follow_path(path,height,cell_length):
+    draw_on_display("Drone Starting!!!")                #  x  +  > forward
+    take_off(height)                                    #  y  +  > go right
+                            # 0 -> x                       z  +  > go down
+    prev_cord = path[0]     # 1 -> y                      yaw 60 > turn right 60 degrees
+    degree = 0
+    for cord in path[1:]:
+        if cord[0] == prev_cord[0] and cord[1] > prev_cord[1]:          # Go forward
+            degree = point_forward(degree)
+            go_forward()
+        elif cord[0] == prev_cord[0] and cord[1] < prev_cord[1]:        # Go backwards
+            degree = point_backwards(degree)
+            go_forward()
+        elif cord[0] < prev_cord[0] and cord[1] == prev_cord[1]:        # Turn left, go 1 forward
+            degree = point_left(degree)
+            go_forward()
+        elif cord[0] > prev_cord[0] and cord[1] == prev_cord[1]:        # Turn right, go 1 forward
+            degree = point_right(degree)
+            go_forward()
+        elif cord[0] > prev_cord[0] and cord[1] > prev_cord[1]:         # Go forward 1, right 1
+            degree = point_forward(degree)
+            go_forward()
+            degree = point_right(degree)
+            go_forward()
+        elif cord[0] < prev_cord[0] and cord[1] > prev_cord[1]:         # Go forward 1, left 1
+            degree = point_forward(degree)
+            go_forward()
+            degree = point_left(degree)
+            go_forward()
+        elif cord[0] > prev_cord[0] and cord[1] < prev_cord[1]:         # Go backward 1, right 1
+            degree = point_backwards(degree)
+            go_forward()
+            degree = point_right(degree)
+            go_forward()
+        elif cord[0] < prev_cord[0] and cord[1] < prev_cord[1]:         # Go backward 1, left 1
+            degree = point_backwards(degree)
+            go_forward()
+            degree = point_left(degree)
+            go_forward()
+
+        prev_cord = cord    # set new prev_cord
+    land()
+
+class LoRaRcvCont(LoRa):
+    def __init__(self, verbose=False):
+        super(LoRaRcvCont, self).__init__(verbose)
+        self.set_mode(MODE.SLEEP)
+        self.set_dio_mapping([0, 0, 0, 0, 0, 0])
+
+    def start(self):
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT)
+
+        while True:
+            time.sleep(.5)
+            status = self.get_modem_status()
+
+    def on_rx_done(self):
+        self.clear_irq_flags(RxDone=1)
+        self.set_modem_config_1()
+        payload = self.read_payload(nocheck=True)
+        data =remove_null_bytes(bytearray(payload).decode('utf-8', 'ignore').strip())
+
+        if 'correct' in data: # Correct data is received and path is ready
+            data = dict(data)
+            follow_path(data['path'],data['height'],data["cell_length"])    # Start route
+
+        self.set_mode(MODE.SLEEP)
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT)
+
+
+
+draw_on_display("Connecting..")
+try:
+    vehicle = connectMyCopter()
+except:
+    draw_on_display("ERROR!")
+    exit(0)
+
+try:
+    lora = LoRaRcvCont(verbose=False)
+    lora.set_mode(MODE.STDBY)
+    lora.set_pa_config(pa_select=1)
+
+    draw_on_display("READY!")
+    lora.start()
+except KeyboardInterrupt:
+    print("\nKeyboardInterrupt")
+except:
+    draw_on_display("ERROR LoRa!")
+    exit(0)
+finally:
+    lora.set_mode(MODE.SLEEP)
+    BOARD.teardown()
